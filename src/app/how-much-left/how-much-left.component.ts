eeb3c471f4c388bf35524b890bfd8e7f62208843
Ricard Mallafré López <rikitzzz@gmail.com>
2020-05-31 11:55:15 +0200

    calculations are now done! next steps: make it a pwa and deploy on firebase

--- src/app/how-much-left/how-much-left.component.ts
+++ src/app/how-much-left/how-much-left.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { WeekDay } from '@angular/common';
 
 @Component({
   selector: 'app-how-much-left',
@@ -7,10 +8,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HowMuchLeftComponent {
   public doNotCountToday: boolean = false;
-  public amount: number = 0;
+  public totalAmountLeft: number = 0;
+  public nextPayDay: Date;
+  public differenceInDays: number = 0;
+  public averageToSpendPerDay: string;
+
+  public static readonly MS_IN_DAY = 24 * 60 * 60 * 1000;
   constructor() {}
 
-  calculate(event: KeyboardEvent): void {
-    this.amount = parseFloat((event.target as HTMLInputElement).value);
+  calculate(val?: string): void {
+    this.totalAmountLeft = parseFloat(val);
+
+    if (isNaN(this.totalAmountLeft)) {
+      console.log('Not a number');
+      this.totalAmountLeft = 0;
+      return;
+    }
+
+    this.nextPayDay = this.findNextPayDay();
+    this.differenceInDays = this.dayDifference();
+    this.averageToSpendPerDay = (
+      this.totalAmountLeft / this.differenceInDays
+    ).toFixed(2);
+  }
+
+  findNextPayDay() {
+    const now = new Date();
+    now.setHours(0, 0, 0, 0);
+    //getMonth() returns a 0-based index, but the month in new Date() requires
+    //a 1-based index
+    let monthNextPayDay =
+      now.getDate() < 24 ? now.getMonth() + 1 : now.getMonth() + 2;
+    let yearNextPayDay;
+    if (monthNextPayDay <= 12) {
+      yearNextPayDay = now.getFullYear();
+    } else {
+      monthNextPayDay = 1;
+      yearNextPayDay = now.getFullYear() + 1;
+    }
+    const nextPayDay = new Date(
+      yearNextPayDay + '/' + monthNextPayDay + '/' + 24
+    );
+    while (
+      nextPayDay.getDay() === WeekDay.Saturday ||
+      nextPayDay.getDay() === WeekDay.Sunday
+    ) {
+      nextPayDay.setDate(nextPayDay.getDate() - 1);
+    }
+
+    return nextPayDay;
+  }
+
+  dayDifference() {
+    const now = new Date();
+    if (this.doNotCountToday)
+      now.setTime(now.getTime() + HowMuchLeftComponent.MS_IN_DAY);
+
+    const diff = Math.ceil(
+      (this.nextPayDay.getTime() - now.getTime()) /
+        HowMuchLeftComponent.MS_IN_DAY
+    );
+
+    return diff;
   }
 }
