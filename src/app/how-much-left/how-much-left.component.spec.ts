5b6be42c60dcc4cffec760447d8d95feb2abfb54
nikens-MBP16 <rikitzzz@gmail.com>
2020-12-05 20:43:16 +0100

    feat: adding firebase auth with nebular

--- src/app/how-much-left/how-much-left.component.spec.ts
+++ src/app/how-much-left/how-much-left.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { HowMuchLeftComponent } from './how-much-left.component';
 
@@ -6,7 +6,7 @@ describe('HowMuchLeftComponent', () => {
   let component: HowMuchLeftComponent;
   let fixture: ComponentFixture<HowMuchLeftComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HowMuchLeftComponent ]
     })
