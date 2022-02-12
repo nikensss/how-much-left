c17483bdbde523321d500107be44c9edb287fc42
nikens-MBP16 <rikitzzz@gmail.com>
2022-02-12 12:59:32 +0100

    fix: use preferred pay day from user's preferences

--- src/app/how-much-left/how-much-left.component.ts
+++ src/app/how-much-left/how-much-left.component.ts
@@ -4,13 +4,7 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AmountDocument } from '../interfaces/AmountDocument.interface';
 import { NbToastrService } from '@nebular/theme';
-import { FieldValue, serverTimestamp } from 'firebase/firestore';
-
-interface AmountLeft {
-  uid: string;
-  amount: number;
-  date: FieldValue;
-}
+import { serverTimestamp } from 'firebase/firestore';
 
 @Component({
   selector: 'app-how-much-left',
@@ -25,9 +19,11 @@ export class HowMuchLeftComponent implements OnInit {
   public canSave = false;
   public canPop = false;
 
-  private unsubscribe: () => void;
+  private unsubscribe: (() => void)[] = [];
 
   public static readonly MS_IN_A_DAY = 24 * 60 * 60 * 1000;
+
+  private preferredPayDay = 24;
 
   constructor(
     public auth: AngularFireAuth,
@@ -39,24 +35,31 @@ export class HowMuchLeftComponent implements OnInit {
   ngOnInit(): void {
     this.canSave = false;
     this.auth.onAuthStateChanged((user) => {
-      if (!user && !this.unsubscribe) return;
-
-      if (!user && this.unsubscribe) {
-        this.unsubscribe();
-        this.unsubscribe = null;
+      if (!user) {
+        for (const unsubscribe of this.unsubscribe) {
+          unsubscribe();
+        }
         return;
       }
 
       this.db.collection<AmountDocument>(`users/${user.uid}/amounts`, (ref) => {
         const result = ref.orderBy('date');
-        this.unsubscribe = result.onSnapshot((snapshot) => {
-          this.ngZone.run(() => {
-            if (snapshot.metadata.hasPendingWrites) return;
-            this.canPop = snapshot.docs.length > 0;
-          });
-        });
+        this.unsubscribe.push(
+          result.onSnapshot((snapshot) => {
+            this.ngZone.run(() => {
+              if (snapshot.metadata.hasPendingWrites) return;
+              this.canPop = snapshot.docs.length > 0;
+            });
+          })
+        );
         return result;
       });
+
+      this.db
+        .doc<{ payDay: number }>(`users/${user.uid}/preferences/general`)
+        .get()
+        .toPromise()
+        .then((v) => (this.preferredPayDay = v.data().payDay));
     });
   }
 
@@ -71,10 +74,10 @@ export class HowMuchLeftComponent implements OnInit {
   get nextPayDay() {
     const now = this.now;
     now.setHours(0, 0, 0, 0);
-    const PAY_DATE = 24;
+    let payDay = this.preferredPayDay;
     //getMonth() returns a 0-based index, but the month in new Date() requires
     //a 1-based index, that's why either 1 or 2 needs to be added.
-    let month = now.getMonth() + (now.getDate() < PAY_DATE ? 1 : 2);
+    let month = now.getMonth() + (now.getDate() < payDay ? 1 : 2);
     let year = now.getFullYear();
     if (month > 12) {
       month = 1;
@@ -82,7 +85,8 @@ export class HowMuchLeftComponent implements OnInit {
     }
     const nextPayDay = new Date(`${year}/${month}/${PAY_DATE}`);
 
-    while (nextPayDay.getDay() > WeekDay.Saturday) {
+
+    while ([WeekDay.Saturday, WeekDay.Sunday].includes(nextPayDay.getDay())) {
       nextPayDay.setDate(nextPayDay.getDate() - 1);
     }
 
